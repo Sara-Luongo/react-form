03c873a9529f711ffd549bdaff776bcc760c1b0f
Sara-Luongo <saraluongo202002@gmail.com>
2026-05-04 17:37:26 +0200

    creato array di oggetti per articoli con la map creao la card per ogni articolo

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,38 @@
 import HelloClasse154 from "./components/HelloClasse154";
+const listaArticoli = [
+  {
+    id: 1,
+    intitolazione: 'titolo',
+    descrizione: 'descrizione'
 
+  },
+  {
+    id: 2,
+    intitolazione: 'titolo',
+    descrizione: 'descrizione'
+
+  },
+  {
+    id: 3,
+    intitolazione: 'titolo',
+    descrizione: 'descrizione'
+
+  }
+]
+
+function App() {
+  return <>
+    {listaArticoli.map(articolo => {
+      const { id, intitolazione, descrizione } = articolo
+      return <div key={id} nameclass="card">
+        <div nameclass="card-body">
+          <h1 nameclass="card-title">{intitolazione}</h1>
+          <p>{descrizione}</p>
+        </div>
+
+      </div>
+
+    })}
+  </>
+}
 export default App;
